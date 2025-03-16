f937c5401103685c83ec2e6d416733a620db36b0
Adrian Kari <adrka@itu.dk>
2025-03-16 15:38:03 +0100

    Added new doors

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private bool isLocked = true;
+    [SerializeField] private GameObject door;
+
+    private void Close()
+    {
+
+    }
+
+    private void Open()
+    {
+
+    }
+}
