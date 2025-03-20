0dbcaa0cf331abdb0525623a616039a98b60195e
Adrian Kari <adrka@itu.dk>
2025-03-20 13:40:10 +0100

    Added sound to electrical box

--- Assets/Scripts/ElectricalPanel.cs
+++ Assets/Scripts/ElectricalPanel.cs
@@ -1,9 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ElectricalPanel : MonoBehaviour
 {
+    [Header("Audio Settings")]
+    [SerializeField] private AudioClip switchSoundEffect;
+    [SerializeField] private AudioClip poweringUpSound;
+
     public void TurnOn()
     {
         LightFlicker.LightsOn = true;
+        StartCoroutine(PlaySounds());
+    }
+
+    private IEnumerator PlaySounds()
+    {
+        AudioSource.PlayClipAtPoint(switchSoundEffect, transform.position);
+        yield return new WaitForSeconds(0.5f);
+        AudioSource.PlayClipAtPoint(poweringUpSound, transform.position, 0.5f);
     }
 }
